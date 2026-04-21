fcef73ba533a5822ab6eefda3174d392bff8ee0d
Md. Talim <talimmohammad116@gmail.com>
2026-04-21 17:05:08 +0530

    Add initial Spring Boot project with Maven wrapper

---
+++ src/main/java/me/mdtalim/botguard/BotguardApplication.java
@@ -0,0 +1,13 @@
+package me.mdtalim.botguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotguardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BotguardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/mdtalim/botguard/BotguardApplicationTests.java
@@ -0,0 +1,13 @@
+package me.mdtalim.botguard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotguardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
